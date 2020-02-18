db8d61b42700f1aea194e5be61db9cabb4132755
Nathalia <nathaliamelare@gmail.com>
2020-02-18 14:20:16 -0300

    Object added and plane being detected

--- NajupAR/NajupAR/ViewController.swift
+++ NajupAR/NajupAR/ViewController.swift
@@ -12,64 +12,61 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
-    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet var sceneView: TestARSCNView!
+    
+    let coaching = Coaching()
+    
+    //class ARPlaneAnchor: ARAnchor
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
+        //sceneView.showsStatistics = true
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        let bloco = SCNScene(named: "art.scnassets/bloco_2x2.scn")!
+        sceneView.scene = bloco
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        setupCoaching()
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        configuration.planeDetection = [.horizontal, .vertical]
+        
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        print("New Plane Anchor found at extent:", planeAnchor.extent)
+        
     }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-        
     }
     
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    }
+    
+    func setupCoaching() {
+        coaching.setup(sceneView: sceneView)
+        coaching.addCoaching()
     }
 }
